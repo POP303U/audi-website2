48abd445a2e9e4d468fe14fcf0bc7ec39312d995
POP303U <nicoanton.gering@gmail.com>
2023-11-08 14:12:52 +0100

    Moved images into image_wheel

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const images = ["image1.png", "image2.png", "image3.png"]; // Replace with your image file names
-let currentIndex = 0;
+const images = ["image1.png", "image2.png", "image3.png"]; // Replace with your image file names let currentIndex = 0;
 
 const button = document.getElementById("changeButton");
 button.addEventListener("click", function(event){
