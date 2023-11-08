63df6efd509f664a823dd4b40f40761af01b84a6
POP303U <nicoanton.gering@gmail.com>
2023-11-08 11:29:45 +0100

    nearly finished

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-console.log("hello guys")
+const images = ["image1.png", "image2.png", "image3.png"]; // Replace with your image file names
+let currentIndex = 0;
 
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-}
+const button = document.getElementById("changeButton");
+button.addEventListener("click", function(event){
+    const imageElement = document.getElementById("image");
+    imageElement.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+});
